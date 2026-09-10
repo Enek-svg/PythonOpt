1093c6e5c53321690ba4d637b6ae5058f64baef0
Enek-svg <enekionic1932@gmail.com>
2026-09-10 11:55:58 +0200

    V0.1 añadido str, int float bool que es

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+# Print sirve para mostrar por pantalla el texto que le indiquemos.
+
+print("Soy Eneko")
+print("Estoy en SMR2")
+print("Estoy aprendiendo Python")
+
+nombre = "Eneko"
+numero = 1
+numero_decimal = 1.5
+# str : cadenas de texto
+# int : números enteros
+# float : números decimales
+# bool : valores booleanos (True o False)
