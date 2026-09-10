5ebb524ad91d20871bc11b22c307da68a6f84e05
Enek-svg <enekionic1932@gmail.com>
2026-09-10 13:17:10 +0200

    pruebas

---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+print("Hola ",nombre, " tienes ", str(edad), " años") # las comas hacen que funcione como un espacio entre las palabras, si no se ponen las comas, se juntan todas las palabras y no se entiende nada.
