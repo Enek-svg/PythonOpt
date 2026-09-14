a1134ed23d1bce170d7e8900e4d9a72a539300b9
Enek-svg <enekionic1932@gmail.com>
2026-09-14 12:09:20 +0200

    con float y sin el operaciones

--- ejercicio3.py
+++ ejercicio3.py
@@ -13,3 +13,11 @@ base = input("Dame una base: ")
 altura = input("Dame una altura: ")
 print("El area es: " + str(int(base) * int(altura)))
 print("El perimetro es: " + str(2 * int(base) + 2 * int(altura))) 
+
+base2 = float(input("Dame una base: "))
+altura2 = float(input("Dame una altura: "))
+
+area2= base2*altura2
+print(f"El area es: {area2}")
+perimetro2 = 2*base2 + 2*altura2
+print(f"El perimetro es: {perimetro2}")
