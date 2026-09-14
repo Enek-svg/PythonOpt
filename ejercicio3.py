202ff59f31716047e6767b8d0bc456a75f3be18f
Enek-svg <enekionic1932@gmail.com>
2026-09-14 12:03:47 +0200

    operaciones ejercicio

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,5 +1,5 @@
 # OPERACIONES
-"""""
+""""
 print(5+3)
 print(5-3)
 print (3-5)
@@ -7,6 +7,9 @@ print(5*3)
 print(5**3) 
 print(5/3)
 print(5//3)
-""""
+"""
 
-
+base = input("Dame una base: ")
+altura = input("Dame una altura: ")
+print("El area es: " + str(int(base) * int(altura)))
+print("El perimetro es: " + str(2 * int(base) + 2 * int(altura))) 
