ae09c93c4c9f04dc778076f9f96dd9ed227b4de6
Enek-svg <enekionic1932@gmail.com>
2026-09-17 12:42:12 +0200

    espacios en ej 9

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,12 +1,15 @@
 print("Hola soy Enek \n tengo 17 años y soy de Barakaldo")
 nombre = input("Introduce tu nombre: ")
 print("Hola", nombre, "Bienvenido")
+
 numero1 = int(input("Introduce un número: "))
 numero2 = int(input("Introduce otro número: "))
 print("La suma de", numero1, "y", numero2, "es:", numero1 + numero2)
+
 edad = int(input("Introduce tu edad ej(2008):"))
 edad2 = 2026 - edad
 print(edad2)
+
 print("este texto se pondra en grande".upper())
 letras = input("Dime una palabra: ")
 print("La primera letra de esa palabra es: " + letras[0])
