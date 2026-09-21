d9cf643ce258b03c82e2b3a9bfb18db322f83067
Enek-svg <enekionic1932@gmail.com>
2026-09-21 12:02:19 +0200

    nada

--- ejercicio8.py
+++ ejercicio8.py
@@ -7,14 +7,14 @@ correo = input("Ingrese su correo electrónico: ")
 # Ejercicio 8. El programa pedirá: nombre, apellido, edad, ciudad y correo. Con esos datos debe mostrar una ficha ordenada, incluyendo un campo Usuario generado automáticamente: la primera letra del nombre + el apellido en minúsculas ejemplo: (Ane Gil → agil). 
 
 # EJERCICIO CORRECTO
-#print("=============================== \n CARNÉ DIGITAL \n===============================")
-#print("Nombre: " + nombre)
-#print("Apellido: " + apellido)
-#print("Edad: " + edad)
-#print("Ciudad: " + ciudad)
-#print("Correo electrónico: " + correo)
-#print("Usuario: " + nombre[0].lower() + apellido.lower())
-#print("===============================")
+print("=============================== \n CARNÉ DIGITAL \n===============================")
+print("Nombre: " + nombre)
+print("Apellido: " + apellido)
+print("Edad: " + edad)
+print("Ciudad: " + ciudad)
+print("Correo electrónico: " + correo)
+print("Usuario: " + nombre[0].lower() + apellido.lower())
+print("===============================")
 
 # EJERCICIO + EXTRA DE CORREO ELECTRÓNICO
 print("=============================== \n CARNÉ DIGITAL \n===============================")
