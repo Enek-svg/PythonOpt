0bd22b63e3129534127c605b14ef7aa473f19e15
Enek-svg <enekionic1932@gmail.com>
2026-09-24 12:34:48 +0200

    nada

--- if elif else/lista comandos explicados segundo tema.py
+++ if elif else/lista comandos explicados segundo tema.py
@@ -1,4 +1,16 @@
+# IF ELIF ELSE
 # El if es para evaluar una condición y ejecutar un bloque de código si la condición es verdadera.
 # Elif se utiliza para evaluar múltiples condiciones, y se ejecuta el bloque de código correspondiente a la primera condición verdadera.
 # Los Elif puedes poner todos los que quieras
 # Else se utiliza para ejecutar un bloque de código si ninguna de las condiciones anteriores es verdadera.
+
+# OPERADORES COMPARATIVOS
+# Son operadores que comparan valores y devuelven un valor booleano (True o False)
+# print(4 < 8) # menor que
+# print(4 > 8) # mayor que
+# print(4 <= 8) # menor o igual que
+# print(4 >= 8) # mayor o igual que
+# print(4 == 8) # igual que
+# print(4 != 8) # diferente que
+# = asignar
+# == comparar
