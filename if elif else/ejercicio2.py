ee795b7fde2a2537c868c2ccc964dca8a5d632d0
Enek-svg <enekionic1932@gmail.com>
2026-09-24 12:51:05 +0200

    ejercicio 2

---
+++ if elif else/ejercicio2.py
@@ -0,0 +1,8 @@
+numero = 4
+numero2 = float(input("Ingrese un número: "))
+if numero2 > numero:
+    print("El primer número es mayor que el segundo.")
+elif numero2 < numero:
+    print("El primer número es menor que el segundo.")
+else:
+    print("Has conseguido adivinar el numero!")
